0d5da512862d042cd246dcfe549b6c6620d5df46
Ha Cuong Thinh <thinhhacuong123@gmail.com>
2024-05-11 10:20:45 +0700

    photorouter fix

--- routes/PhotoRouter.js
+++ routes/PhotoRouter.js
@@ -5,38 +5,32 @@ const router = express.Router();
 
 router.post("/", async (req, res) => {});
 
-router.get("/:id", async (req, res) => {
+router.get('/:id', async function (request, response) {
   try {
-    const id = req.params.id;
-    const photos = await Photos.find({ user_id: id });
-    if (photos.length === 0) {
-      return res.status(404).send("Photos not found for user");
-    }
-    const processedPhotos = photos.map(async (photo) => {
-      delete photo.__v;
-      const commentsWithUsers = await Promise.all(
-        photo.comments.map(async (comment) => {
-          const user = await Users.findOne({ _id: comment.user_id });
-          if (user) {
-            const { location, description, occupation, __v, ...rest } = user;
-            return { ...comment, user: rest };
-          } else {
-            console.warn(
-              ` User ${comment.user_id} not found for comment `
-            );
-            return comment;
-          }   
-        })
-      );
-      photo.comments = commentsWithUsers;
-      return photo;
-    });
-    const results = await Promise.all(processedPhotos);
-    res.json(results);
-  } catch (err) {
-    console.error(err); 
-    res.status(500).send(JSON.stringify("Internal Server Error"));
+      var id = request.params.id;
+      const photos = await Photos.find({ user_id: id });
+      if (!photos || photos.length === 0) {
+          console.log(`** Photos for user with id ${id}: Not Found! **`);
+          return response.status(400).json({message: "NOT FOUND"});
+      }
+      const listPhotos = JSON.parse(JSON.stringify(photos))
+      for (let photo of listPhotos) {
+          delete photo.__v;
+          for (let comment of photo.comments) {
+              console.log(comment)
+              const user = await Users.findOne({ _id: comment.user_id });
+              if (user) {
+                  const { location, description, occupation, __v, ...rest } = user.toJSON();
+                  comment["user"] = rest;
+              }
+          }
+      }
+      response.json(listPhotos);
+  } catch (error) {
+      console.error("Error:", error);
+      response.status(500).send("Internal Server Error");
   }
 });
+
 
 module.exports = router;
